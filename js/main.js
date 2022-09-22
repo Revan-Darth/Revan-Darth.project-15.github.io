8ad89f97361be704f93964e0cba60125693992eb
Revan-Darth <nitro.Knight3080@gmail.com>
2022-09-22 21:54:41 +0700

    Added Header & Adaptive

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.querySelector('.menu__burger').addEventListener('click', function (e){
+	e.preventDefault();
+	
+	if(this.classList.contains('active')) {
+	   this.classList.remove('active');
+	   document.body.classList.remove('lock');
+	   document.querySelector('.header__menu').classList.remove('active');
+	} else {
+	   this.classList.add('active');
+	   document.body.classList.add('lock');
+	   document.querySelector('.header__menu').classList.add('active');
+	}
+});
